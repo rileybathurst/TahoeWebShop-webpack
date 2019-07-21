ef0cb323130c90c75463cf44936e9882c211c09a
Riley Bathurst <riley@rileybathurst.com>
2019-07-21 16:22:09 -0700

    scroll snap on ios and a few other things

--- src/app.js
+++ src/app.js
@@ -1,1 +1,59 @@
 import _ from 'lodash';
+
+// IntersectionObserver to rotate the text around the profile image
+if ('IntersectionObserver' in window &&
+'IntersectionObserverEntry' in window &&
+'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+
+  let iopush = document.querySelectorAll('.turn');
+
+  var numSteps = 20.0;
+
+  var pusher; // naming choice is too close to push
+  var prevRatio = 0.0;
+  var pT = "rotate(-Ratiodeg)";
+
+  // Set things up.
+
+  window.addEventListener("load", function(event) {
+  pusher = document.querySelectorAll(".turn");
+
+  createObserver();
+  }, false);
+
+  function createObserver() {
+  var observer;
+
+  var options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: buildThresholdList()
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+
+  pusher.forEach(pushers => {
+    observer.observe(pushers);
+  });
+  }
+
+  function buildThresholdList() {
+  var thresholds = [];
+  var numSteps = 100;
+
+  for (var i=1.0; i<=numSteps; i++) {
+  var ratio = i/numSteps;
+  thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+  }
+
+  function handleIntersect(entries, observer) {
+  entries.forEach(function(entry) {
+    entry.target.style.webkitTransform = pT.replace("Ratio", (entry.intersectionRatio*75));
+    prevRatio = entry.intersectionRatio;
+  });
+  }
+}
